ac76fc30f32304763f669bb4ab1735c296643b29
Lee-Tanghui <leetanghui424@gmail.com>
2017-09-25 16:40:16 +0800

    初始化代码

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+      <product-list-one v-bind:products="products"></product-list-one>
+      <product-list-two v-bind:products="products"></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
+
+export default {
+    name: 'app',
+    components: {
+        'product-list-one': ProductListOne,
+        'product-list-two': ProductListTwo
+    },
+    data () {
+        return {
+            products: [
+                {name: '鼠标', price: 20},
+                {name: '键盘', price: 40},
+                {name: '耳机', price: 60},
+                {name: '显示屏', price: 80}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+body{
+    font-family: Ubuntu;
+    color: #555;
+}
+</style>
